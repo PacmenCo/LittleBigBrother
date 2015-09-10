f147810ee9690ca36fe6ca4972ce2475305c06da
PacmenCo <Rasmus@Rasmuss-MacBook-Pro.local>
2015-09-10 17:18:17 +0200

    fdfd fddf
    dfdf

--- app/src/main/java/com/pervasivecomputing/pervasivecomputing/MainActivity.java
+++ app/src/main/java/com/pervasivecomputing/pervasivecomputing/MainActivity.java
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     RelativeLayout background;
     Button mapButton;
 
+    private int uselessInt;
+
     private CallbackManager callbackManager;
     private TextView info;
     private LoginButton loginButton;
